b776e935790d60f2e7b40fc064245564d2dc2109
Vignesh <realvignesh@gmail.com>
2025-04-11 14:44:05 -0700

    feat: initial commit

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Orcajet.dev";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = inputs.nixpkgs.lib.systems.flakeExposed;
+
+      perSystem = {pkgs, ...}: {
+        devShells.default = pkgs.mkShell {
+          name = "hello-svelte";
+          buildInputs = with pkgs; [
+          ];
+        };
+      };
+    };
+}
